6756add289b7f3257409b0ef6dee6403babf5691
sriharsh <sriharsh.gampa@outlook.com>
2023-07-15 11:36:36 +0530

    added help feature

--- task.js
+++ task.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+const args = process.argv.slice(2);
+
+let usage = `Usage :-
+$ ./task add 2 hello world    # Add a new item with priority 2 and text "hello world" to the list
+$ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order
+$ ./task del INDEX            # Delete the incomplete item with the given index
+$ ./task done INDEX           # Mark the incomplete item with the given index as complete
+$ ./task help                 # Show usage
+$ ./task report               # Statistics`;
+
+if (args[0]=="help" || args[0]==null){
+console.log(usage);
+}
